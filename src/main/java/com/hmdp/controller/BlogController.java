04f29c25440f75c1fedc7890f2683256a4b839f8
dy <dyu32345@gmail.com>
2023-11-03 21:05:35 +0800

    根据用户 id, 分页查询他的博客

--- src/main/java/com/hmdp/controller/BlogController.java
+++ src/main/java/com/hmdp/controller/BlogController.java
@@ -105,6 +105,24 @@ public class BlogController {
     }
 
 
+    /**
+     * 根据用户 id, 分页查询他的博客
+     *
+     * @param current
+     * @param id
+     * @return
+     */
+    @GetMapping("/of/user")
+    public Result queryBlogByUserId(
+            @RequestParam(value = "current", defaultValue = "1") Integer current,
+            @RequestParam("id") Long id) {
+        // 根据用户查询
+        Page<Blog> page = blogService.query()
+                .eq("user_id", id).page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        return Result.ok(records);
+    }
 
 
 }
